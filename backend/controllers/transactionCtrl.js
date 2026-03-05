2cfb6c4a9bc0c47653d09b28edff3cf2d99edc67
Tejas-824 <tejaswinigmo@gmail.com>
2026-03-05 18:43:02 +0530

    Add required date in Transaction model

--- backend/controllers/transactionCtrl.js
+++ backend/controllers/transactionCtrl.js
@@ -3,7 +3,7 @@ const Category = require("../model/Category");
 const Transaction = require("../model/Transaction");
 
 const transactionController = {
-  //!add
+  //add
   create: asyncHandler(async (req, res) => {
     const { type, category, amount, date, description } = req.body;
     if (!amount || !type || !date) {
@@ -15,6 +15,7 @@ const transactionController = {
       type,
       category,
       amount,
+      date,
       description,
     });
     res.status(201).json(transaction);
@@ -25,12 +26,13 @@ const transactionController = {
     const { startDate, endDate, type, category } = req.query;
     let filters = { user: req.user };
 
-    if (startDate) {
-      filters.date = { ...filters.date, gte: new Date(startDate) };
-    }
-    if (endDate) {
-      filters.date = { ...filters.date, lte: new Date(endDate) };
-    }
+   if (startDate) {
+   filters.date = { ...filters.date, $gte: new Date(startDate) };
+  }
+
+  if (endDate) {
+  filters.date = { ...filters.date, $lte: new Date(endDate) };
+  }
     if (type) {
       filters.type = type;
     }
--- backend/model/Transaction.js
+++ backend/model/Transaction.js
@@ -23,6 +23,7 @@ const transactionSchema = new mongoose.Schema(
     },
     date: {
       type: Date,
+      required: true,
       default: Date.now,
     },
     description: {
